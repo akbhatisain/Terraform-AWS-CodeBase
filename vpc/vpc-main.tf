400550f45b9721fa84c459f35f5e3bc899678fd0
akbhatisain <aksainbhati@gmail.com>
2024-09-07 01:51:16 +0530

    service modify

--- vpc/vpc-main.tf
+++ vpc/vpc-main.tf
@@ -1,11 +1,11 @@
-provider "aws" {
-  region = "us-west-1"                                  # Change this to your preferred AWS region
-}
-
 module "vpc" {
-  source             = "../modules/VPC"
-  cidr_block         = "10.0.0.0/16"                    # Choose your Network range.
-  vpc_name           = "my-vpc"                         # Change it accordingly.
-  availability_zones = ["us-west-1a", "us-west-1b"]     # You can go with one availability zone. 
-  public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]  # You can go with one subnet if you are using single availability zone.
+  source = "./modules/VPC"
+
+  vpc_name                = "my-vpc"
+  vpc_cidr                = "10.0.0.0/16"
+  public_subnet_cidrs    = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnet_cidrs   = ["10.0.3.0/24", "10.0.4.0/24"]
+  public_subnet_count     = 2
+  private_subnet_count    = 2
+  availability_zones      = data.aws_availability_zones.available.names
 }
